7985d24f681e11aab216ce0a57c0105d16f2b2bb
enrique novoa <luisenriquenovoa@gmail.com>
2018-06-27 22:29:41 -0700

    2nd

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
+var vertexShaderText =
+[
+  'precision mediump float;',
+  '',
+  'attribute vec2 vertPosition;',
+  '',
+  'void main()',
+  '{',
+  '  gl_Position = vec4(vertPosition, 0.0, 1.0);',
+  '}'
+].join('\n');
+
+var fragmentShaderText =
+[
+  'precision mediump float;',
+  '',
+  'void main()',
+  '{',
+  '  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);',
+  '}'
+].join('\n');
+
 var InitDemo = function() {
-  console.log("this is working");
+  console.log(vertexShaderText);
 
   var canvas = document.getElementById('game-surface');
   var gl = canvas.getContext('webgl');
@@ -17,5 +39,14 @@ var InitDemo = function() {
   // gl.viewport(0, 0, window.innerWidth, window.innerHeight);
 
   gl.clearColor(0.75, 0.85, 0.8, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+  gl.shaderSource(vertexShader, vertexShaderText);
+  gl.shaderSource(fragmentShader, fragmentShaderText);
+
+  gl.compileShader(vertexShader);
+  gl.compileShader(fragmentShader);
 };
